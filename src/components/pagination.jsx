f753ac34c50f14441b09322eabaecc66d6ce9f66
Wei Hung <wayne930242@gmail.com>
2022-05-26 21:21:41 +0800

    Style home

---
+++ src/components/pagination.jsx
@@ -0,0 +1,14 @@
+const Pagination = ({
+  Pagination,
+  setPagination,
+  total,
+  limit = 10,
+}) => {
+
+  return (
+    <div>
+
+
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,9 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
+  const LIMIT = 10
+  const [pagination, setPagination] = React.useState(0)
+
   if (posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
@@ -30,36 +33,38 @@ const BlogIndex = ({ data, location }) => {
       <Seo title="All posts" />
       <Bio />
       <ol style={{ listStyle: `none` }}>
-        {posts.map(post => {
-          const title = post.frontmatter.title || post.fields.slug
+        {posts
+          .slice(pagination * LIMIT + 1, (pagination + 1) * LIMIT)
+          .map(post => {
+            const title = post.frontmatter.title || post.fields.slug
 
-          return (
-            <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
-            </li>
-          )
-        })}
+            return (
+              <li key={post.fields.slug}>
+                <article
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+                >
+                  <header>
+                    <h2>
+                      <Link to={post.fields.slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                    <small>{post.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+                </article>
+              </li>
+            )
+          })}
       </ol>
     </Layout>
   )
